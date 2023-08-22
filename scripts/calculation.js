6deea0cd811578ffd7a296c5eb9d302355bff9e7
saad200224 <saad19021994@outlook.com>
2023-08-22 07:50:43 +0600

    solved

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -10,52 +10,32 @@ function handleClick(target){
     total = parseInt(total) + parseInt(price);
     document.getElementById("total-span").innerText = total.toFixed(2);
 
-    return total;
+    const netTotal = document.getElementById('net-total');
+    netTotal.innerText = total.toFixed(2);
 
-    // const inputField = document.getElementById('coupon-input');
-    // const inputValue = inputField.value;
+    return total.toFixed(2);
+}
 
-    // if (total >= 200){
-    //     const discount = total * 0.2;
-    //     const discountField = document.getElementById('discount-span');
-    //     discountField.innerText = discount.toFixed(2);
+function couponButton(){
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
 
-    //     const netTotal = document.getElementById('net-total');
-    //     netTotal.innerText = (total - discount).toFixed(2);
+    if ( inputValue === 'SELL200' && total >= 200){
+        const discount = (total * 0.2).toFixed(2);
+        document.getElementById('discount-span').innerText = discount;
 
-    // }else{
-    //     const discount = total * 0;
-    //     const discountField = document.getElementById('discount-span');
-    //     discountField.innerText = discount.toFixed(2);
+        const netTotal = document.getElementById('net-total');
+        netTotal.innerText = (total.toFixed(2)-discount).toFixed(2);
+        
+    } else{
+        const discount = (total * 0).toFixed(2);
+        document.getElementById('discount-span').innerText = discount;
 
-    //     const netTotal = document.getElementById('net-total');
-    //     netTotal.innerText = (total - discount).toFixed(2);
-    // }
+        const netTotal = document.getElementById('net-total');
+        netTotal.innerText = (total.toFixed(2)-discount).toFixed(2);
+    } 
 }
 
 function goHome(){
     window.location.href = 'index.html'
-}
-
-function couponButton(){
-
-    const inputField = document.getElementById('coupon-input');
-    const inputValue = inputField.value;
-
-    if ( inputValue === 'SELL200'){
-        const discount = total * 0.2;
-        const discountField = document.getElementById('discount-span');
-        discountField.innerText = discount.toFixed(2);
-
-    }else{
-        const discount = total * 0;
-        const discountField = document.getElementById('discount-span');
-        discountField.innerText = discount.toFixed(2);
-    }  
-}
-
-
-
-function discountCalculation(){
-
 }
